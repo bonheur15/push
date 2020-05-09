4769d6792ae14d82dcd12619560118488ac21dc8
unknown <rumanzibonheur@gmail.com>
2020-05-08 22:38:59 -0700

    fix

--- push.js
+++ push.js
@@ -4,5 +4,5 @@ let vapidKeys = {
   privateKey: 'ahWd7rS9udVuZmUZ6ntmdgsCGZ0ePI_4dsNfrtqgJYw'
 }
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/c05894XAxXU:APA91bHq2g4UMuDW_O4Sr4yRvS9dP-89m1a2o5FpsVrfrx24N_8OrREyIQJ8Oz1G3hRwB5tfLMxBynZKLPW4s6BcR_kXKGrU3ghO_508r3CuDV_9b9IjcM6w-FS4cd-r45KL4jGtb9SX","expirationTime":null,"keys":{"p256dh":"BPhOYcvAPRErnmxFbtbkOj06Y0GRd7Us7lc50cbOFG_PLk6uGXRs4-t48iE5-RnylYL-ezx0FK1zAnj2Wo1CkPM","auth":"7efpsdyO1F-Be14zd2uiRw"}}
+let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/exNKosa8w3s:APA91bFt5N_EfagIVMTRG3h1OmVffbs5w5sUW6Xs-5eyhMdfCP84AgMhk-ePwGMUkLRkKKSrd4HDOXVgjSzdDHK4ObdIVFrGoi6DF6kwIXe7vA-GcULAjpnw3SoZwd-jm5KEFLBEFEup","expirationTime":null,"keys":{"p256dh":"BIsK1JHJ3PlCwvtb2goSdQw92OBZ1g_NggiUptOViQ8S5XZvQX0_halxJ8HTvm-znSt65n9Ai-WrcbaYNFQOSpY","auth":"J032Hqw66qLtH2SFXMjUQw"}}
 push.sendNotification(sub, 'test message')
