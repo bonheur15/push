f0b811932368df276d61d98b71f7d4aea25bf821
unknown <rumanzibonheur@gmail.com>
2020-05-08 22:13:27 -0700

    fix

--- push.js
+++ push.js
@@ -1,10 +1,8 @@
-var push,testpu = require('web-push');
+var push = require('web-push');
 let vapidKeys = {
   publicKey: 'BI2ess2eBzVCebnRTqQVZ-D8qqmb-VGDoCeppAzcFyZz2qX9RCoe61XZamsVpAJUGPAypeQZVHI2-UZ9DnQ7Hbk',
   privateKey: 'ahWd7rS9udVuZmUZ6ntmdgsCGZ0ePI_4dsNfrtqgJYw'
 }
 push.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
-testpu.setVapidDetails('mailto:test@code.co.uk', vapidKeys.publicKey, vapidKeys.privateKey)
-let sub = {"endpoint":"https://fcm.googleapis.com/fcm/send/d6kCC1DrbQs:APA91bHLlWC40RX6KvLY6UxWt8jBN-2ab0GHepnihZ5rXSiY7v_uQbIDxTgO3gGXEnrTKLlFNcVWPXhwZiapO-kGRM_6ydTudl9dai1bB77yFLKjB7VeDH-UpOLFr6OvieO203_oPNjC","expirationTime":null,"keys":{"p256dh":"BB3IDaz6FayJ0D5EWDk0o6ZGtALYmTPUfiG4LSdxJcxvtSD126bPLCLHQ-yu01NKB6GPLLjdPwH-gQtpolBSoXo","auth":"Vx00WlCdfOzKECBDdJOseg"}}
-//push.sendNotification(sub, 'test message')
-testpu.sendNotification(sub, 'test message')
+let sub = { "endpoint": "https://fcm.googleapis.com/fcm/send/dz_H07azIoE:APA91bFt4Fg3kE1UbnaGYooCHGGpQHMMXfe_9rT1NfJ202IYhd2EBy_lf-pSaZsW9tZ41UHDpHzQpca_A1AUlRssGxjiCxegEUgQHKyksVVZL5PeXwhKsFdo_6JBoN4k1RwEAr5H6HJZ", "expirationTime": null, "keys": { "p256dh": "BLbJxwWYUXN8PWZnf5ih2UB5MI20qvt1l4eLc6jNzW_hvuy-2BAeWKlXhT-dQGKdGDCrjRez_eNNKh7DI4JKRRs", "auth": "98pIyH92MBLXI1U6qaDYWw" } }
+push.sendNotification(sub, 'test message')
--- sw.js
+++ sw.js
@@ -19,23 +19,3 @@ self.addEventListener('push', function(e) {
     );
   });
 
-  self.addEventListener('testpu', function(e) {
-    var options = {
-      body: 'Tsjdhkjadssssssssssssssshkadm a push!',
-      icon: 'images/example.png',
-      vibrate: [100, 50, 100],
-      data: {
-        dateOfArrival: Date.now(),
-        primaryKey: '2'
-      },
-      actions: [
-        {action: 'explore', title: 'Explore this new world',
-          icon: 'images/checkmark.png'},
-        {action: 'close', title: 'Close',
-          icon: 'images/xmark.png'},
-      ]
-    };
-    e.waitUntil(
-      self.registration.showNotification('Hello world!', options)
-    );
-  });
